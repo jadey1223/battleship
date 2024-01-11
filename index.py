cf53a3c359349030e0c0e1f14984a5d2a2bd1b82
jadey1223 <142165000+jadey1223@users.noreply.github.com>
2024-01-11 09:02:08 +0000

    grid system in place

--- index.py
+++ index.py
@@ -1,7 +1,14 @@
 from random import randint
 
 #Board for holding ship locations
-HIDDEN_BOARD = [[" "] * 8 for x in range(8)]
+HIDDEN_BOARD = [[" "] * 10 for x in range(10)]
 # Board for displaying hits and misses
-GUESS_BOARD = [[" "] * 8 for i in range(8)]
+GUESS_BOARD = [[" "] * 10 for i in range(10)]
 
+def print_board(board):
+    print("  A B C D E F G H I J")
+    print("  *-*-*-*-*-*-*-*-*-*")
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
