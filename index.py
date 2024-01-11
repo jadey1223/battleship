963bc799c98506996555e3d7de0c942abba69bc2
jadey1223 <142165000+jadey1223@users.noreply.github.com>
2024-01-11 08:57:07 +0000

    two player features

--- index.py
+++ index.py
@@ -1,1 +1,7 @@
 from random import randint
+
+#Board for holding ship locations
+HIDDEN_BOARD = [[" "] * 8 for x in range(8)]
+# Board for displaying hits and misses
+GUESS_BOARD = [[" "] * 8 for i in range(8)]
+
