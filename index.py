9370d7be171827186d3bfbebb7c58778fbd3f101
jadey1223 <142165000+jadey1223@users.noreply.github.com>
2024-01-11 09:17:57 +0000

    addressed alphabet with number

--- index.py
+++ index.py
@@ -12,3 +12,15 @@ def print_board(board):
     for row in board:
         print("%d|%s|" % (row_number, "|".join(row)))
         row_number += 1
+        
+letters_to_numbers = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7
+}
+
